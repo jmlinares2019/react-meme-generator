002a2cc643b5bbf48676e5afcd6f589020bd24b0
jmlinares2019 <jlinaresmar@gmail.com>
2024-07-19 14:22:20 +0200

    Adding getting random meme image functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,47 @@
+import { useState } from 'react'
+import memes from "../assets/memesData"
+
 function Form(){
+
+    const [ memeImgUrl, setMemeImgUrl ] = useState("");
+
+    function getRandomMeme(){
+        const allMemes = memes.data.memes;
+        const totalMemes = allMemes.length;
+        let randomNumber = Math.random();
+        let randomMemeId = Math.floor(totalMemes * randomNumber);
+        let randomMeme = allMemes[randomMemeId].url;
+        setMemeImgUrl(randomMeme);
+    }
+
     return(
-        <form className="meme-form">
+        <div className="meme-form">
             <div className="row inputs">
                 <div className="col-6">
                     <label htmlFor="top-text" className="form-label">Top text</label>
-                    <input type="text" name="top-text" className="form-control"/>
+                    <input 
+                        type="text"
+                        name="top-text" 
+                        className="form-control"   
+                    />
                 </div>  
                 <div className="col-6">
                     <label htmlFor="bottom-text" className="form-label">Bottom text</label>
-                    <input type="text" name="bottom-text" className="form-control" />
+                    <input 
+                        type="text" 
+                        name="bottom-text" 
+                        className="form-control"  
+                    />
                 </div>
             </div>
             <div className="row image-button">
                 <div className="col">
-                    <button type="submit" className="btn">Get a new meme image</button>
+                    <button className="btn form-button" onClick={getRandomMeme}>Get a new meme image</button>
                 </div>
             </div>
-        </form>
+            <img src={memeImgUrl} alt="" />
+        </div>
+        
     )
 }
 
