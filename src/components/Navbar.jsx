45d362b1c32928392eeb3aac1d45987121dca96c
jmlinares2019 <jlinaresmar@gmail.com>
2024-07-22 15:51:55 +0200

    Adding year, copyright and name to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,14 @@ import React from "react";
 import logo from "../assets/img/logo.png"
 
 function Navbar(){
+    
+    let year = new Date().getFullYear()
+
     return(
        <nav className="navbar">
             <h1 className="visually-hidden">Meme Generator</h1>
             <img src={logo} />
-            <span className="metadata">React Course - Project 3</span>
+            <span className="metadata">{ year } © Juan Manuel Linares</span>
        </nav>
        
     )
